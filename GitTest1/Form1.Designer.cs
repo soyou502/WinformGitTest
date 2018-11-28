244a5ab1aacc703dffad43bd0f497676376bb2df
soyou502 <45410668+soyou502@users.noreply.github.com>
2018-11-28 11:08:50 +0900

    Update Form1.Designer.cs

--- GitTest1/Form1.Designer.cs
+++ GitTest1/Form1.Designer.cs
@@ -6,7 +6,7 @@
         /// 필수 디자이너 변수입니다.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
+//연습(풀)
         /// <summary>
         /// 사용 중인 모든 리소스를 정리합니다.
         /// </summary>
