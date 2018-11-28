8f2cd7490c5f507c85ef5d1ce10cdb0da4ce7bde
IcJsh <soyou501@gmail.com>
2018-11-28 10:47:38 +0900

    버튼+클릭이벤트

--- GitTest1/Form1.cs
+++ GitTest1/Form1.cs
@@ -16,5 +16,10 @@ namespace GitTest1
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
